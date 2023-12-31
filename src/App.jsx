df82664c4f1340ad645e53a5e6df6e5b0a16340c
Mahmud <rainbowmahmud@gmail.com>
2023-12-31 19:18:52 +0600

    initial layout addded.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import news from "./data/samakal.json"
+import { useState } from "react";
+import "./App.css";
+// import news from "./data/samakal.json";
+import news from "./data/manabzamin.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div class="xl:container mx-auto">
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-6 gap-4">
+          {news.map((item, i) => {
+            const hasImgSrc = item.hasOwnProperty("imgSrc");
+            const hasExcerpt = item.hasOwnProperty("excerpt");
+            const hasTime = item.hasOwnProperty("time");
+            let classes;
 
-          {
-            news.map(item => (
+            if (hasImgSrc && hasExcerpt) classes = "col-span-2 row-span-2";
+            else if (hasExcerpt) classes = "row-span-2";
+            else if (hasImgSrc) classes = "col-span-2";
+            else classes = "cols-span-1 row-span-1";
+
+            const bgImage = hasImgSrc && item.imgSrc;
+
+            return (
               <>
-                <div className="card w-full bg-base-100 shadow-xl">
-                  <div className="card-body">
-                    <h2 className="h4">{item.title}</h2>
-                    <p>{item?.excerpt}</p>
-                    <div className="card-actions justify-end">
-                      <div className="badge badge-neutral">{item?.time}</div>
-                    </div>
+                <div className={`card bg-base-100 shadow-xl ${classes}`}>
+                  <div
+                    className={`card-body bg-cover bg-center ${
+                      hasImgSrc && "bg-[url(" + item.imgSrc + ")]"
+                    }`}
+                    // style={`background-image: url(${item?.imgSrc})`}
+                    // style="background-image: url(https://samakal.com/media/imgAll/2023December/4-1703682537.jpg)"
+                  >
+                    <h2 className="h2">{item.title}</h2>
+                    {hasExcerpt && <p>{item.excerpt}</p>}
+
+                    {hasTime && (
+                      <div className="card-actions justify-end">
+                        <div className="badge badge-neutral">{item.time}</div>
+                      </div>
+                    )}
                   </div>
                 </div>
               </>
-            ))
-          }
+            );
+          })}
 
           {/* <div className="grid grid-cols-subgrid gap-4 col-span-3">
             <div className="col-start-3">06</div>
@@ -34,7 +53,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
