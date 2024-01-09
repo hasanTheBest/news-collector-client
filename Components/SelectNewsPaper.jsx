cc373b3c47431898a3440d7e5fef53f946fa0cf1
Mahmud <rainbowmahmud@gmail.com>
2024-01-09 15:45:01 +0600

    Filter section ui added.

--- Components/SelectNewsPaper.jsx
+++ Components/SelectNewsPaper.jsx
@@ -11,45 +11,77 @@ import {
 import newsPaperUrls from "../src/data/dummyData/newsUrls.json";
 import { useState } from "react";
 import { useNewspaper } from "./NewspaperContext";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+const filterSections = ["Leading", "Latest", "Most Read", "National", "International", "Editorial/Opnion", "Sports", "Entertainment", "Job"]
 
 const SelectNewsPaper = () => {
   const { selectedUrls, handleSelectNewspaper, handleNewspaperSubmission } =
     useNewspaper();
 
+  const [value, setValue] = React.useState(filterSections[0].toLowerCase());
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <React.Fragment>
       <Grid container direction="column" mb={6}>
-        {Object.entries(newsPaperUrls).map(([key, value]) => {
-          const items = value.map(({ title, url }) => (
-            <FormControlLabel
-              key={Math.random()}
-              control={<Checkbox />}
-              label={title}
-              value={url}
-              onChange={() => handleSelectNewspaper(url)}
-              checked={selectedUrls.indexOf(url) > -1}
-            />
-          ));
+        <Grid container direction="row">
+          <Grid container direction="column" item md={9} flexGrow={1}>
+            {Object.entries(newsPaperUrls).map(([key, value]) => {
+              const items = value.map(({ title, url }) => (
+                <FormControlLabel
+                  key={Math.random()}
+                  control={<Checkbox />}
+                  label={title}
+                  value={url}
+                  onChange={() => handleSelectNewspaper(url)}
+                  checked={selectedUrls.indexOf(url) > -1}
+                />
+              ));
 
-          return (
-            <Grid item xs key={key}>
-              <Box>
-                <Typography
-                  variant="h5"
-                  color="primary"
-                  sx={{ textTransform: "uppercase" }}
-                >
-                  {key}
-                </Typography>
-                <FormGroup>
-                  <Box sx={{ display: "inline-flex", flexWrap: "wrap" }}>
-                    {items}
+              return (
+                <Grid item xs key={key}>
+                  <Box>
+                    <Typography
+                      variant="h5"
+                      color="primary"
+                      sx={{ textTransform: "uppercase" }}
+                    >
+                      {key}
+                    </Typography>
+                    <FormGroup>
+                      <Box sx={{ display: "inline-flex", flexWrap: "wrap" }}>
+                        {items}
+                      </Box>
+                    </FormGroup>
                   </Box>
-                </FormGroup>
-              </Box>
-            </Grid>
-          );
-        })}
+                </Grid>
+              );
+            })}
+          </Grid>
+          {/* Filter option */}
+          <Grid item md={3}>
+            <FormControl>
+              <FormLabel id="demo-controlled-radio-buttons-group" className="h6">Sections</FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={value}
+                onChange={handleChange}
+              >
+                {filterSections.map((item, i) => (
+                  <FormControlLabel key={Math.random()} value={item.toLowerCase()} control={<Radio />} label={item} />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+        </Grid>
 
         {/* Submit button */}
         <Box
