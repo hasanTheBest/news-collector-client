19af737f1c11a0a93d0af8606f0c6de570cab9e2
Mahmud <rainbowmahmud@gmail.com>
2024-02-14 15:29:34 +0600

    snackbar added but not functioned

--- utilites/ShowResponseAlert.jsx
+++ utilites/ShowResponseAlert.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
 import { useNewspaper } from '../Context/NewspaperContext'
 import { favicons, getHostName } from './faviconsConfig'
-import { Alert } from '@mui/material'
+import { Alert, Snackbar } from '@mui/material'
 
 const ShowResponseAlert = () => {
   const { newsData } = useNewspaper()
+  const [open, setOpen] = React.useState(false);
 
-  if (newsData?.type === 'error') {
-    return (
-      <Alert severity="error" variant="outlined">{`${favicons[getHostName(newsData?.url)]["title"]}`}</Alert>
-    )
-  }
+  const handleClick = () => {
+    setOpen();
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  // if(newsData && newsData.url){
+  //   if( newsData.url)
+  // }
 
   return (
-    <Alert severity="success" variant="outlined">{`${favicons[getHostName(newsData?.url)]["title"]} is loaded successfully.`}</Alert>
+    // <Alert severity="success" variant="outlined">{`${newsData?.name} \n ${newsData?.message}`}</Alert>
+    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={newsData?.type === 'success' ? "success" : "error" }
+          variant="filled"
+          sx={{ width: '50%' }}
+        >
+          {
+            newsData?.type === 'success' ? `<b>${favicons[getHostName(newsData?.url)]["title"]}</b> is loaded successfully` : `Error: ${newsData?.name} \n ${newsData?.message}`
+          }
+        </Alert>
+      </Snackbar>
   )
 }
 
